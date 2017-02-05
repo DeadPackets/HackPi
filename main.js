ddc55c7e6482c55c22d20b22629d56f00e9afe85
Youssef Awad <shadowblade7536@gmail.com>
2017-02-05 20:43:56 +0400

    Update main.js

--- main.js
+++ main.js
@@ -21,9 +21,48 @@ TODO:
 */
 var os = require('os');
 var wifi = require('wifi');
-/*
-Insert HTTP code which I dont know here
+var express = require('express');
+const colors = require('colors');
+var app = express();
+var http = require('http')
+var https = require('https')
+var fs = require('fs');
+var auth = require('basic-auth');
+//var port = 443;
+var httpport = 80;
+/*var options = {
+    key: fs.readFileSync('privkey/goes/here/privkey.pem'),
+    cert: fs.readFileSync('cert/goes/here/cert.pem')
+};
+const mysql = require('mysql');
+var sql = mysql.createConnection({host: 'localhost', user: 'root', password: 'mypass', database: 'mydb'});
+
+//Prevents MySQL Database from dying
+setInterval(function() {
+    sql.query('SELECT 1');
+}, 10000);
+
 */
+//Logging functions
+var log = {
+    error: function(data) {
+        var date = new Date();
+        console.log('ERROR'.red, data);
+    },
+    info: function(data) {
+        var date = new Date();
+        console.log('INFO'.green, data);
+    },
+    warn: function(data) {
+        var date = new Date();
+        console.log('WARN'.yellow, data);
+    },
+    debug: function(data) {
+        console.log('DEBUG'.blue, data);
+    }
+}
+
+//Functions
 function GetInterfaceInfo(){
 
 }
@@ -35,3 +74,59 @@ function GetUptime(){
 function ListHostapdClients(){
   
 }
+
+//Starting up HTTP and HTTPS
+//var server = https.createServer(options, app).listen(port, function() {
+//    log.info("Express server listening on port " + port);
+//});
+
+var serverhttp = http.createServer(app).listen(httpport, function() {
+    log.info("Express server listening on port " + httpport);
+});
+serverhttp.listen(80);
+
+//SOCKET.IO INIT
+var io = require('socket.io')(serverhttp) //CHANGE TO SECURE LATER
+
+/*app.use(function(req, res, next) {
+    if (req.secure) {
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url); //req.headers.host
+        log.debug("Redirected" + req.connection.remoteAddress + " to HTTPS.");
+    }
+});*/
+
+app.use(express.static(__dirname + '/web'));
+
+app.get('/', function(req, res) {
+    log.debug(req.connection.remoteAddress + " GET /")
+    res.sendFile('file');
+});
+
+/*
+//HTTP AUTH EXAMPLE
+app.get('/auth', function(req, res) {
+    var credentials = auth(req)
+    if (!credentials || credentials.name !== 'username' || credentials.pass !== 'password') {
+      res.statusCode = 401
+      res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+      res.end('Access denied')
+    } else {
+      log.debug(req.connection.remoteAddress + " GET /auth")
+      res.sendFile('files');
+  }
+});
+*/
+
+//Custom 404
+app.use(function(req, res) {
+    res.send('404: Page not Found').status(404);
+    log.warn(req.connection.remoteAddress + " [404] GET " + req.url)
+});
+
+io.on('connection', function(socket, next) {
+    log.info(socket.handshake.address + " has connected.")
+})
+
