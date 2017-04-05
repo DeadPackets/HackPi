a3072852b9a4f4c82b2422fc9840768357ac70e4
Ubuntu <shadowblade7536@gmail.com>
2017-04-05 14:05:03 +0000

    Updated scripts (light fix)

--- scripts/genCerts.sh
+++ scripts/genCerts.sh
@@ -2,8 +2,11 @@
 
 echo "Remember the passphrase you set here!"
 echo "This script assumes you are inside the directory scripts/ !!"
-sleep 2
-cd ../ssl/
+echo "WARNING: If you arent in the scripts/ directory, it wont work!"
+sleep 5
+cd ../
+mkdir ssl
+cd ssl/
 openssl req -new > new.ssl.csr
 sudo openssl rsa -in privkey.pem -out server.key
 sudo openssl x509 -in new.ssl.csr -out server.cert -req -signkey server.key -days 365
--- scripts/getTools.sh
+++ scripts/getTools.sh
@@ -3,3 +3,8 @@
 echo "Starting to get tools, this may take a while...."
 sleep 2
 sudo apt update
+sudo apt install macchanger crunch 
+cd ../
+mkdir tools
+cd tools/
+
