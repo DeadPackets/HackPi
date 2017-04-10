019553bb0eb21c34bde5dd58e9aacca970816ade
Youssef Awad <shadowblade7536@gmail.com>
2017-04-10 10:42:49 +0400

    Create sniff.js

---
+++ server/sniff.js
@@ -0,0 +1,15 @@
+var pcap = require('pcap2')
+var interface = 'eth0'
+var ARP_PACKETS = new pcap.Session(interface, { filter: 'arp' } )
+var TCP_PACKETS = new pcap.Session(interface, { filter: 'tcp' } )
+var UDP_PACKETS = new pcap.Session(interface, { filter: 'udp' } )
+
+TCP_PACKETS.on('packet', function(rawPacket){console.log("TCP")})
+
+ARP_PACKETS.on('packet', function(rawPacket){ 
+//console.log(pcap.decode.packet(rawPacket).payload.payload); 
+console.log("ARP")
+})
+
+UDP_PACKETS.on('packet', function(rawPacket){console.log("UDP")})
+Contact GitHub API Training Shop Blog About
