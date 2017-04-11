15e83262adb0eb428cd5f0ca98adbee0592f3cb0
Youssef Awad <shadowblade7526@gmail.com>
2017-04-11 04:43:28 +0000

    Working on bluetooth

--- server/functions/bluetooth.js
+++ server/functions/bluetooth.js
@@ -1,6 +1,10 @@
 import Scanner from 'bluetooth-scanner';
 import bluetooth from 'bluetooth';
 
+export const ListBluetoothIfaces = (cb) => {
+	//exec hciconfig here
+}
+
 export const BluetoothState = (iface, cb) => {
 	//todo using bluetooth package
 }
@@ -9,12 +13,13 @@ export const BluetoothToggle = (iface, cb) => {
 	//todo using bluetooth package
 }
 
-export const BluetoothToggle = (iface, cb) => {
-	//todo using bluetooth package
+export const DoSBluetooth = (iface, device, cb) => {
+	//Denial of service for bluetooth here	
 }
 
-
-
+export const BlueoothPing = (iface, device, cb) => {
+	//Ping certain bluetooth device (maybe find approx distance?)
+}
 
 export const ScanBluetooth = (iface, cb) => {
 	bleScanner = new Scanner(iface, cb, (mac, name) => {
